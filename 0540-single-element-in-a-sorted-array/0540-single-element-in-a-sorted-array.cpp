503b109161a1e1b36a17d271165e34e5bd75bc14
harshitjha2001 <85453454+harshitjha2001@users.noreply.github.com>
2022-10-28 23:36:37 +0530

    Time: 66 ms (36.64%), Space: 22.4 MB (10.87%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,34 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        
-        int si = 0, ei = nums.size()-1;
-        while(si <ei){
-            int mid = (si + ei)/2;
-            if( (mid % 2 == 0 && nums[mid] == nums[mid +1]) || (mid %2 == 1 && nums[mid]== nums[mid - 1]) )
-                si = mid + 1;
+class Solution
+{
+    public:
+        int singleNonDuplicate(vector<int> &nums)
+        {
+
+            
+            int si = 0, ei = nums.size() - 1;
+            if(ei==0)
+                return nums[0];
+            if (nums[1] != nums[0])
+                return nums[0];
+
+            if (nums[ei] != nums[ei - 1])
+                return nums[ei];
+
+            while (si <= ei)
+            {
+                int mid = si + (ei - si) / 2;
+
+                if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                    return nums[mid];
+
+                if (((mid % 2) == 0 && nums[mid] == nums[mid + 1]) or((mid % 2) == 1) && nums[mid] == nums[mid - 1])
+            si = mid + 1;
+
             else
-                ei = mid;
+                ei = mid - 1;
         }
-        return nums[si];
-        
-    }
+
+    return -1;
+
+}
 };
