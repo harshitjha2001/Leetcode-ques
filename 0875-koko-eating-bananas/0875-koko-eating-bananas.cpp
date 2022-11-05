0bc1ae92cee993e9d5f736053d0f462eccd445be
harshitjha2001 <85453454+harshitjha2001@users.noreply.github.com>
2022-11-05 10:59:22 +0530

    Time: 48 ms (93.73%), Space: 18.9 MB (34.50%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         
-        int l = 1, r = 1000000000;
+        int l = 1, r = 1e9;
         while (l < r) {
             int m = (l + r) / 2, total = 0;
             for (int p : piles)
