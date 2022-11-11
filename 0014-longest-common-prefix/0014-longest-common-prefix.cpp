ade9ccd6bf8126a9a8a6e29c59821ff0588f210c
harshitjha2001 <85453454+harshitjha2001@users.noreply.github.com>
2022-11-12 01:27:03 +0530

    Time: 11 ms (40.23%), Space: 9.2 MB (53.65%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -9,38 +9,38 @@ class Solution
             {
                 return strs[0];
             }
-            int idx=INT_MAX;
-            int y=INT_MAX;
-            int mlen= min(strs[0].length(), strs[1].length());
+            int idx = INT_MAX;
+            int y = INT_MAX;
+            int mlen = min(strs[0].length(), strs[1].length());
             for (int i = 0; i < strs.size() - 1; i++)
             {
-                int x=0;
+                int x = 0;
                 int len = min(strs[0].length(), strs[i + 1].length());
                 cout << len << " ";
-                
-                if(strs[i+1]=="")
+
+                if (strs[i + 1] == "")
                 {
                     return "";
                 }
-              
+
                 for (int j = 0; j < len; j++)
                 {
-                    
+
                     if (strs[0][j] != strs[i + 1][j])
                     {
                         idx = min(j, idx);
                         break;
                     }
                     else
-                    {x++;
-                    idx=min(y,x); }
+                    {
+                        x++;
+                        idx = min(y, x);
+                    }
                 }
-                y=idx;
-          
-                   
+                y = idx;
             }
             cout << idx;
-          
+
             return strs[0].substr(0, idx);
         }
 };
