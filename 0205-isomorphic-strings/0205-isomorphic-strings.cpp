5fd5fdb82f96ee600b4598537237d1448752bea7
harshitjha2001 <85453454+harshitjha2001@users.noreply.github.com>
2022-11-13 01:00:13 +0530

    Time: 32 ms (8.57%), Space: 6.9 MB (91.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -6,9 +6,9 @@ public:
         
         for(int i=0;i<s.length();i++){
             
-            if(m1.count(s[i])>0 && m1[s[i]]!=t[i])
+            if(m1.count(s[i]) && m1[s[i]]!=t[i])
                 return false;
-            if(m2.count(t[i])>0 && m2[t[i]]!=s[i])
+            if(m2.count(t[i]) && m2[t[i]]!=s[i])
                 return false;
             m1[s[i]] = t[i];
             m2[t[i]] = s[i];
