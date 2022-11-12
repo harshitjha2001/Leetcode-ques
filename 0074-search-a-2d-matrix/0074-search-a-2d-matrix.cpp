ef98f57a39aac16aaf7a93e0a91b1afd2c83ce95
harshitjha2001 <85453454+harshitjha2001@users.noreply.github.com>
2022-11-13 00:16:00 +0530

    Time: 7 ms (70.24%), Space: 9.5 MB (51.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,45 +4,43 @@ class Solution
         bool searchMatrix(vector<vector < int>> &matrix, int target)
         {
 
-           	//         int row= matrix.size();
-           	//         int col=matrix[0].size();
+           	        int row= matrix.size();
+           	        int col=matrix[0].size();
 
-           	//         int low=0;
-           	//         int high=(row*col)-1;
+           	        int low=0;
+           	        int high=(row*col)-1;
 
-           	//         while(low<=high)
-           	//         {
-           	//             int mid = ((high+low)/2);
-           	//             if(matrix[mid/col][mid % col] == target) {   	//mid/col = req row  &mid%col= req col
-           	//                 return true;
-           	//             }
-           	//             if(matrix[mid/col][mid % col] < target) {
-           	//                 low = mid + 1;
-           	//             }
-           	//             else {
-           	//                 high = mid - 1;
-           	//             }
-           	//          }
+           	        while(low<=high)
+           	        {
+           	            int mid = ((high+low)/2);
+           	            if(matrix[mid/col][mid % col] == target) {    	//mid/col = req row  &mid%col= req col
+           	                return true;
+           	            }
+           	            if(matrix[mid/col][mid % col] < target) {
+           	                low = mid + 1;
+           	            }
+           	            else {
+           	                high = mid - 1;
+           	            }
+           	         }
 
-           	//         return false;
+           	        return false;
 
-            int row = 0;
-            int col = matrix[0].size()-1;
-            
-            while(row<matrix.size()  && col>= 0)
-            {
-                if(matrix[row][col]==target)
-                    return true;
-                
-                else if(matrix[row][col]<target)
-                    row++;
-                
-                else
-                    col--;
-            }
-            
-            return false;
-            
-            
+//             int row = 0;
+//             int col = matrix[0].size() - 1;
+
+//             while (row < matrix.size() && col >= 0)
+//             {
+//                 if (matrix[row][col] == target)
+//                     return true;
+
+//                 else if (matrix[row][col] < target)
+//                     row++;
+
+//                 else
+//                     col--;
+//             }
+
+//             return false;
         }
 };
